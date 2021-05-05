1b7ac7cc078d4a09a955c5b128511dbb821cddc5
KyDevtw <k3030789@gmail.com>
2021-05-05 09:43:28 +0800

    itemList

--- cart/admin/add.php
+++ cart/admin/add.php
@@ -1,6 +1,6 @@
 <?php
-require_once('./checkAdmin.php'); // 引入登入判斷
-require_once('../db.inc.php'); // 引用資料庫連線
+require_once('./checkAdmin.php'); //引入登入判斷
+require_once('../db.inc.php'); //引用資料庫連線
 
 //回傳狀態
 $objResponse = [];
